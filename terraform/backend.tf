37764b470859e66f068d4a8733c6ab3999865fd1
FaithKovi <jacinto23github>
2024-07-31 16:42:41 +0100

    update s3 backend

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket         = "terraform-state-bucket1232444231"
-    key            = "./terraform.tfstate"
+    key            = "terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "resume-terraform-locks"
     encrypt        = true
